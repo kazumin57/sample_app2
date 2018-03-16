badb792e836eeed6c406585dfeea48031e4d18da
Kazu Minami <kazu_min57@icloud.com>
2018-03-16 15:42:28 +0000

    Make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, length: { maximum: 20 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
